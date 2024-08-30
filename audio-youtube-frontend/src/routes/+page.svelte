9d4c200942c85f4ba3b0d406b7ecca598b0d0d56
ashwyn28 <ashwyn28annauth@gmail.com>
2024-08-30 02:40:42 +0100

    add searchbar

--- audio-youtube-frontend/src/routes/+page.svelte
+++ audio-youtube-frontend/src/routes/+page.svelte
@@ -1,24 +1,30 @@
 <script>
-    import Player from "$lib/components/Player.svelte"
-    import { search } from "$lib/api/search";
-    import { onMount } from "svelte";
+  import Player from "$lib/components/Player.svelte";
+  import { search } from "$lib/api/search";
+  import { onMount } from "svelte";
 
-    $: videoIds = []
+  $: videoIds = [];
 
-    const handleSearch = async (q) => {
-        const res = await search("mkbhd");
-        if (res) {
-            videoIds = res.videos
-        }
+  const handleSearch = async (q) => {
+    const res = await search("mkbhd");
+    if (res) {
+      videoIds = res.videos;
     }
+  };
 
-    onMount(async () => {
-        await handleSearch()
-    })
+  onMount(async () => {
+    await handleSearch();
+  });
 </script>
 
+<label class="input input-bordered flex items-center gap-2">
+  <input type="text" class="grow" placeholder="Search" />
+  <kbd class="kbd kbd-sm">⌘</kbd>
+  <kbd class="kbd kbd-sm">K</kbd>
+</label>
+
 {#if videoIds}
-    {#each videoIds as videoId}
-        <Player {videoId}/>
-    {/each}
+  {#each videoIds as videoId}
+    <Player {videoId} />
+  {/each}
 {/if}
