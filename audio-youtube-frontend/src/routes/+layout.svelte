05a3710b62680999a2da88031bba3a9f4a0ea092
ashwyn28 <ashwyn28annauth@gmail.com>
2024-10-27 18:31:47 +0000

    add logo

--- audio-youtube-frontend/src/routes/+layout.svelte
+++ audio-youtube-frontend/src/routes/+layout.svelte
@@ -2,6 +2,13 @@
   import "../app.css";
 </script>
 
-<div class="relative min-h-screen p-[7%] md:pb-[7%] pb-[25%]">
-  <slot />
+<div class="relative min-h-screen">
+  <div class="flex justify-start p-5">
+    <img src="/audio-youtube-icon-light.svg" class="block dark:hidden" alt="Audio YouTube Icon" />
+    <img src="/audio-youtube-icon-dark.svg" class="hidden dark:block" alt="Audio YouTube Icon" />
+    <h1 class="text-lg font-bold content-center px-2">AudioTube</h1>
+  </div>
+  <div class="p-[7%] md:pb-[7%] pb-[25%]">
+    <slot />
+  </div>
 </div>
